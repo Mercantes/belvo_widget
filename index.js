b7871602da535f8eae13cc246deec808fd9da1a2
Vinicius Mercante <vinicius.mercante@v4company.com>
2025-03-30 12:44:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.use(express.json());
 const BELVO_ID = process.env.BELVO_ID;
 const BELVO_PASSWORD = process.env.BELVO_PASSWORD;
 
+console.log("BELVO_ID:", BELVO_ID);
+console.log("BELVO_PASSWORD:", BELVO_PASSWORD ? "OK" : "FALTANDO");
+
 app.post('/gerar-widget', async (req, res) => {
   const { external_id } = req.body;
+  
 
   try {
     const accessRes = await axios.post('https://sandbox.belvo.com/api/token/', {
